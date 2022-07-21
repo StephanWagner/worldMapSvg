2dde59db3d420c3bfea9910bd422b20a81081d9c
Stephan Wagner <stephanwagner.me@gmail.com>
2022-07-21 22:23:43 +0200

    Add combined maps, error messages

---
+++ src/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+  combine: {
+    // Australia
+    "AU-ML": "AU",
+    "AU-TAS": "AU",
+
+    // Caribbean Netherlands
+    "BQ-BO": "BQ",
+    "BQ-SA": "BQ",
+
+    // Kiribati
+    "KI-G": "KI",
+    "KI-L": "KI",
+    "KI-P": "KI",
+
+    // Saint Kitts and Nevis
+    "KN-K": "KN",
+    "KN-N": "KN",
+
+    // Papua New Guinea
+    "PG-ML": "PG",
+    "PG-BA": "PG",
+
+    // United States Minor Outlying Islands
+    // TODO center
+    // >> Get the width of the map then move
+    "UM-67": "UM",
+    "UM-71": "UM",
+    "UM-76": "UM",
+    "UM-79": "UM",
+    "UM-81": "UM",
+    "UM-84": "UM",
+    "UM-86": "UM",
+    "UM-95": "UM",
+  },
+};
--- src/map.js
+++ src/map.js
@@ -2,132 +2,180 @@
 
 const fs = require("fs");
 
-try {
-  // Map data
-  const data = fs.readFileSync(__dirname + "/map.svg", "utf8");
+const config = require("./config.js");
 
-  // Counters
-  let countryCount = 0;
-  let borderCount = 0;
-  let combinedMapsCount = 0;
+// Map data
+const data = fs.readFileSync(__dirname + "/map.svg", "utf8");
 
-  // Word map data
-  let worldFileContent = "";
-  let worldMapViewBox = {
-    xMin: 10000,
-    xMax: 0,
-    yMin: 10000,
-    yMax: 0,
-  };
+// Counters
+let errorCount = 0;
+let countryCount = 0;
+let combinedCount = 0;
+let worldCount = 0;
+let borderCount = 0;
 
-  // Regular expression to match country paths
-  const regex =
-    /<path id="path_x5F_([A-Z0-9-]+)[_A-Za-z0-9]*" fill="#[A-Za-z0-9]+" d="([A-Za-z0-9,.\r\n\t\s-]+)"/gu;
+// Word map data
+let worldFileContent = "";
+let worldMapViewBox = {
+  xMin: 10000,
+  xMax: 0,
+  yMin: 10000,
+  yMax: 0,
+};
 
-  // Process country map paths
-  for (const match of data.matchAll(regex)) {
-    // Get the id
-    const id = match[1];
+// Cache to combine maps
+let combineCache = {};
 
-    // Clean up path
-    let path = match[2];
-    path = cleanUpPath(path);
+// Regular expression to match country paths
+const regex =
+  /<path id="path_x5F_([A-Z0-9-]+)[_A-Za-z0-9]*" fill="#[A-Za-z0-9]+" d="([A-Za-z0-9,.\r\n\t\s-]+)"/gu;
 
-    // Get viewBox
-    var viewBox = getViewBox(path);
+// Process country map paths
+for (const match of data.matchAll(regex)) {
+  // Get the id
+  const id = match[1];
 
-    // Update world map viewBox
-    if (viewBox.xMin < worldMapViewBox.xMin) {
-      worldMapViewBox.xMin = viewBox.xMin;
+  // Debug
+  // if (id != "PA") {
+  //   continue;
+  // }
+
+  // Clean up path
+  let path = match[2];
+  path = cleanUpPath(path);
+
+  // Cache combine paths
+  if (config.combine[id]) {
+    const combineId = config.combine[id];
+    if (!combineCache[combineId]) {
+      combineCache[combineId] = [];
     }
-
-    if (viewBox.xMax > worldMapViewBox.xMax) {
-      worldMapViewBox.xMax = viewBox.xMax;
-    }
-
-    if (viewBox.yMin < worldMapViewBox.yMin) {
-      worldMapViewBox.yMin = viewBox.yMin;
-    }
-
-    if (viewBox.yMax > worldMapViewBox.yMax) {
-      worldMapViewBox.yMax = viewBox.yMax;
-    }
-
-    // Generate country file content
-    let countryFileContent = getSvgStart(viewBox);
-    countryFileContent += "\n";
-    countryFileContent += '  <path d="' + path + '"/>';
-    countryFileContent += "\n";
-    countryFileContent += "</svg>";
-    countryFileContent += "\n";
-
-    // Write country file
-    fs.writeFileSync(
-      __dirname + "/../maps/countries/" + id + ".svg",
-      countryFileContent
-    );
-
-    // Generate world file content
-    worldFileContent += '  <path data-map="' + id + '" d="' + path + '"/>';
-    worldFileContent += "\n";
-
-    // Count generated files
-    countryCount++;
-
-    // Country map success message
-    console.log("\x1b[36m", "✓ " + id);
+    combineCache[combineId].push(path);
   }
 
-  // Generate world map file content
-  let worldMapFileContent = getSvgStart(worldMapViewBox);
-  worldMapFileContent += "\n";
-  worldMapFileContent += worldFileContent;
+  // Get viewBox
+  var viewBox = getViewBox(path);
 
-  // Regular expression to match border paths
-  const regexBorders =
-    /<path id="border_x5F_([A-Za-z0-9|_]+)" fill="none" stroke="#[A-Za-z0-9]+" stroke-width="[0-9\.]+" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="([A-Za-z0-9,.\r\n\t\s-]+)"/gu;
-
-  // Process border paths
-  for (const match of data.matchAll(regexBorders)) {
-    const borderPath = cleanUpPath(match[2]);
-    const borderIdsStr = match[1];
-    const borderIds = borderIdsStr.split("_x7C_");
-
-    let borderIdsData = "";
-    borderIds.forEach(function (borderId, index) {
-      borderIdsData += " data-border" + (index + 1) + '="' + borderId + '"';
-    });
-
-    worldMapFileContent +=
-      "  <path" +
-      borderIdsData +
-      ' fill="none" stroke="#FFFFFF" stroke-width="0.05" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="' +
-      borderPath +
-      '"/>';
-    worldMapFileContent += "\n";
-
-    borderCount++;
+  // Update world map viewBox
+  if (viewBox.xMin < worldMapViewBox.xMin) {
+    worldMapViewBox.xMin = viewBox.xMin;
   }
 
-  // Close world map file
-  worldMapFileContent += "</svg>";
+  if (viewBox.xMax > worldMapViewBox.xMax) {
+    worldMapViewBox.xMax = viewBox.xMax;
+  }
+
+  if (viewBox.yMin < worldMapViewBox.yMin) {
+    worldMapViewBox.yMin = viewBox.yMin;
+  }
+
+  if (viewBox.yMax > worldMapViewBox.yMax) {
+    worldMapViewBox.yMax = viewBox.yMax;
+  }
+
+  // Generate country file content
+  let countryFileContent = getSvgStart(viewBox);
+  countryFileContent += "\n";
+  countryFileContent += '  <path d="' + path + '"/>';
+  countryFileContent += "\n";
+  countryFileContent += "</svg>";
+  countryFileContent += "\n";
+
+  // Write country file
+  fs.writeFileSync(
+    __dirname + "/../maps/countries/" + id + ".svg",
+    countryFileContent
+  );
+
+  // Generate world file content
+  worldFileContent += '  <path data-map="' + id + '" d="' + path + '"/>';
+  worldFileContent += "\n";
+
+  // Count generated files
+  countryCount++;
+
+  // Country map success message
+  console.log("\x1b[36m", "✓ " + id);
+}
+
+// Generate combined maps
+
+for (const id in combineCache) {
+  const path = combineCache[id].join(" ");
+  const viewBox = getViewBox(path);
+
+  let combinedFileContent = getSvgStart(viewBox);
+  combinedFileContent += "\n";
+  combinedFileContent += '  <path d="' + path + '"/>';
+  combinedFileContent += "\n";
+  combinedFileContent += "</svg>";
+  combinedFileContent += "\n";
+
+  // Write country file
+  fs.writeFileSync(
+    __dirname + "/../maps/countries/" + id + ".svg",
+    combinedFileContent
+  );
+
+  // Count generated files
+  combinedCount++;
+
+  // Combined country map success message
+  console.log("\x1b[33m", "✓ " + id);
+}
+
+// Generate world map file content
+let worldMapFileContent = getSvgStart(worldMapViewBox);
+worldMapFileContent += "\n";
+worldMapFileContent += worldFileContent;
+
+// Regular expression to match border paths
+const regexBorders =
+  /<path id="border_x5F_([A-Za-z0-9|_]+)" fill="none" stroke="#[A-Za-z0-9]+" stroke-width="[0-9\.]+" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="([A-Za-z0-9,.\r\n\t\s-]+)"/gu;
+
+// Process border paths
+for (const match of data.matchAll(regexBorders)) {
+  const borderPath = cleanUpPath(match[2]);
+  const borderIdsStr = match[1];
+  const borderIds = borderIdsStr.split("_x7C_");
+
+  let borderIdsData = "";
+  borderIds.forEach(function (borderId, index) {
+    borderIdsData += " data-border" + (index + 1) + '="' + borderId + '"';
+  });
+
+  worldMapFileContent +=
+    "  <path" +
+    borderIdsData +
+    ' fill="none" stroke="#FFFFFF" stroke-width="0.05" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="' +
+    borderPath +
+    '"/>';
   worldMapFileContent += "\n";
 
-  // Write world map file
-  fs.writeFileSync(__dirname + "/../maps/world-map.svg", worldMapFileContent);
+  borderCount++;
+}
 
-  combinedMapsCount++;
+// Close world map file
+worldMapFileContent += "</svg>";
+worldMapFileContent += "\n";
 
-  // World map success messages
-  console.log("\x1b[36m", "✓ World map");
+// Write world map file
+fs.writeFileSync(__dirname + "/../maps/world-map.svg", worldMapFileContent);
 
-  // Success messages
-  console.log("\x1b[32m", countryCount + " individual maps generated");
-  console.log("\x1b[32m", borderCount + " borders added to world map");
-  console.log("\x1b[32m", combinedMapsCount + " combined maps generated");
-} catch (err) {
-  // Error message
-  console.error(err);
+worldCount++;
+
+// World map success messages
+console.log("\x1b[33m", "✓ World map");
+
+// Success messages
+console.log("\x1b[32m", "✓ " + countryCount + " individual maps generated");
+console.log("\x1b[32m", "✓ " + combinedCount + " combined maps generated");
+console.log("\x1b[32m", "✓ " + worldCount + " world maps generated");
+console.log("\x1b[32m", "✓ " + borderCount + " borders added to world map");
+
+// Errors
+if (errorCount > 0) {
+  console.log("\x1b[31m", "✗ " + errorCount + " error: See log above");
 }
 
 // Get the start of the SVG with viewBox data
@@ -152,9 +200,9 @@ function getViewBox(d) {
   let xCur = 0;
   let yCur = 0;
   let xMin = 10000;
-  let xMax = 0;
+  let xMax = -10000;
   let yMin = 10000;
-  let yMax = 0;
+  let yMax = -10000;
 
   const regex = /([A-Za-z]+)([0-9-.,]+)/g;
 
@@ -218,5 +266,20 @@ function getViewBox(d) {
 
 // Clean up a path
 function cleanUpPath(path) {
-  return path.replace(/  |\t|\r\n|\n|\r/gm, "");
+  path = path.replace(/  |\t|\r\n|\n|\r/gm, "");
+
+  const paths = path.split(" ");
+
+  paths.forEach(function (pathItem) {
+    if (
+      pathItem.indexOf("l") === -1 &&
+      pathItem.indexOf("h") === -1 &&
+      pathItem.indexOf("v") === -1
+    ) {
+      console.log("\x1b[31m", "✗ Error: Empty path detected");
+      errorCount++;
+    }
+  });
+
+  return path;
 }
